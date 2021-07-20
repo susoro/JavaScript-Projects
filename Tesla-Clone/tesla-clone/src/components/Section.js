00ac3e574451253161653005cf477948a6efdb05
susoro <susoro567@gmail.com>
2021-07-20 23:54:55 +0900

    아...존예다..

---
+++ Tesla-Clone/tesla-clone/src/components/Section.js
@@ -0,0 +1,22 @@
+// rfce
+
+import React from 'react'
+import styled from "styled-components"
+function Section() {
+  return (
+    <Wrap>
+      
+    </Wrap>
+  )
+}
+
+export default Section
+
+const Wrap = styled.div`
+  width:100vw;
+  height:100vh;
+  background-size:cover;
+  background-position:center;
+  background-repeat:no-repeat;
+  background-image:url('/images/model-3.jpg')
+`
