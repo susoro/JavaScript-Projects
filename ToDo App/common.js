f8b25dcba4f7ee64e76f815b878e1a0e2fa4965b
susoro <susoro567@gmail.com>
2021-07-22 20:10:20 +0900

    최종수정

--- ToDo App/common.js
+++ ToDo App/common.js
@@ -1,11 +1,11 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
-const totosUL = document.getElementById("totos");
+const todosUL = document.getElementById("todos");
 
 const todos = JSON.parse(localStorage.getItem("todos"));
 
 if(todos) {
-  todos.forEach(todo => {
+  todos.forEach((todo) => {
     addTodo(todo);
   });
 }
@@ -32,12 +32,13 @@ function addTodo(todo) {
 
     todoEl.innerText = todoText;
 
-    todoEl.addEventListener("click", () =>{
+    todoEl.addEventListener("click", () => {
       todoEl.classList.toggle("completed");
+
       updateLS();
     });
 
-    todoEl.addEventListener("contextmenu", (e)=>{
+    todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
       todoEl.remove();
       updateLS();
@@ -55,11 +56,11 @@ function updateLS(){
   const todoEl = document.querySelectorAll("li");
   const todos = [];
 
-  todoEl.forEach((todoEl)=>{
+  todoEl.forEach((todoEl) => {
     todos.push({
       text: todoEl.innerText,
       completed: todoEl.classList.contains("completed"),
     });
   });
-  localStorage.setItem("todos",JSON.stringify(tpdps));
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
