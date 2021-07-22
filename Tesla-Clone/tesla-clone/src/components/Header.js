6dbf969d79d12ba80b156e510bcc22393229292f
susoro <susoro567@gmail.com>
2021-07-23 00:54:49 +0900

    거의다옴

--- Tesla-Clone/tesla-clone/src/components/Header.js
+++ Tesla-Clone/tesla-clone/src/components/Header.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import styled from "styled-components"
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+
+
 function Header() {
   return (
       <Container>
@@ -8,14 +12,33 @@ function Header() {
         </a>
 
         <Menu>
-          <p><a href="#">Model S</a></p>
-          <p><a href="#">Model Y</a></p>
-          <p><a href="#">Model 3</a></p>
-          <p><a href="#">Model X</a></p>
-          <p><a href="#">Soalr Panels</a></p>
-          <p><a href="#">Solar Roof</a></p>
-          <p><a href="#">Accessories</a></p>
+          <a href="#">Model S</a>
+          <a href="#">Model 3</a>
+          <a href="#">Model X</a>
+          <a href="#">Model Y</a>
         </Menu>
+
+        <RightMenu>
+          <a href="#">Shop</a>
+          <a href="#">Tesla Account</a>
+          <CustomMenu>
+
+          </CustomMenu>
+        </RightMenu>
+        <BurgerNav>
+          <CloseWrapper>
+            <CustomClose>
+            </CustomClose>
+          </CloseWrapper>
+            <li><a href="#">Exsiting Inventory</a></li>
+            <li><a href="#">Used Inventory</a></li>
+            <li><a href="#">Trade-in</a></li>
+            <li><a href="#">cybertruck</a></li>
+            <li><a href="#">Roadaster</a></li>
+            <li><a href="#">Exsiting Inventory</a></li>
+            <li><a href="#">Exsiting Inventory</a></li>
+            <li><a href="#">Exsiting Inventory</a></li>
+        </BurgerNav>
       </Container>
   )
 }
@@ -23,13 +46,79 @@ function Header() {
 export default Header
 
 const Container = styled.div`
+    top:0;
+    left:0;
+    right:0;
     min-height:60px;
     position:fixed;
     display:flex;
+    justify-content:space-between;
     align-items:center;
     padding:0 20px;
+    z-index:1;
 
 `
 const Menu = styled.div`
+    display:flex;
+    align-items:center;
+    flex: 1;
+    justify-content:center;
+
+    a{
+      font-weight:600;
+      text-transform:uppercase;
+      padding:0 10px;
+      flex-wrap:no-wrap;
+    }
+
+    @media(max-width: 768px){
+      display:none;
+    }
+`
+
+const RightMenu = styled.div`
+    display:flex;
+    align-items:center;
+      a{
+      font-weight:600;
+      text-transform:uppercase;
+      margin-right:10px;
+    }
+`
+const CustomMenu = styled(MenuIcon)`
+    cursor:pointer;
+
+`
+const BurgerNav = styled.div`
+    position:fixed;
+    top:0;
+    bottom:0;
+    right:0;
+    background-color:#fff;
+    width:300px;
+    z-index:16;
+    list-style:none;
+    padding:20px;
+    display:flex;
+    flex-direction:column;
+    text-align:start;
     
+    li{
+      padding:15px 0;
+      border-bottom:1px solid rgba(0,0,0,.1);
+
+      a{
+        font-weight:500;
+      }
+
+    }
+`
+
+const CustomClose= styled(CloseIcon)`  
+    cursor:pointer;
+`
+
+const CloseWrapper = styled.div`
+    display:flex;
+    justify-content:flex-end;
 `
--- Tesla-Clone/tesla-clone/src/components/Section.js
+++ Tesla-Clone/tesla-clone/src/components/Section.js
@@ -2,17 +2,21 @@
 
 import React from 'react'
 import styled from "styled-components"
+import Fade from 'react-reveal/Fade';
+
 function Section({ title, description,backgroundImg
 ,leftBtnText,rightBtnText}) {
   return (
     <Wrap bgImage={backgroundImg}>
-      
-      <ItemText>
-          <h1>{ title }</h1>
-          <p>{ description }</p>
-      </ItemText>
+        <Fade bottom>
+          <ItemText>
+            <h1>{ title }</h1>
+            <p>{ description }</p>
+          </ItemText>
+        </Fade>
 
       <Buttons>
+        <Fade bottom>
         <ButtonGroup>
             <LeftButton>
               { leftBtnText }
@@ -23,6 +27,7 @@ function Section({ title, description,backgroundImg
               </RightButton>}
         </ButtonGroup>
         <DownArrow src="/images/down-arrow.svg" />
+        </Fade>
       </Buttons>
 
     </Wrap>
@@ -32,6 +37,7 @@ function Section({ title, description,backgroundImg
 export default Section
 
 const Wrap = styled.div`
+  
   width:100vw;
   height:100vh;
   background-size:cover;
