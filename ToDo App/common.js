b2a4c4528921cb28fbb0a417e28f653eef4d91a5
susoro <susoro567@gmail.com>
2021-07-22 20:17:00 +0900

    Update common.js

--- ToDo App/common.js
+++ ToDo App/common.js
@@ -12,14 +12,12 @@ if(todos) {
 
 form.addEventListener("submit", (e) =>{
   e.preventDefault();
-
   addTodo();
 });
 
 
 function addTodo(todo) {
   let todoText = input.value;
-
   if(todo){
     todoText = todo.text;
   }
@@ -31,10 +29,8 @@ function addTodo(todo) {
     }
 
     todoEl.innerText = todoText;
-
     todoEl.addEventListener("click", () => {
       todoEl.classList.toggle("completed");
-
       updateLS();
     });
 
@@ -45,9 +41,7 @@ function addTodo(todo) {
     });
     todosUL.appendChild(todoEl);
     input.value = "";
-
     updateLS();
-
   }
 
 }
